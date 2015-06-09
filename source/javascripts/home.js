16f566776a8ba86c35caeeb659ee164df2ecc8e2
christopher lemoine <ch_lemoine@yahoo.fr>
2015-06-09 15:12:08 +0200

    ex done except scrollReveal

---
+++ source/javascripts/home.js
@@ -0,0 +1,22 @@
+
+// to adapt the height
+$(document).ready(function() {
+  $(".window-height").css("height", $(window).height());
+});
+
+
+// smooth scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
---
+++ source/javascripts/navbar.js
@@ -0,0 +1,33 @@
+
+
+// to change the navbar
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").css({
+        "background": "rgba(0, 0, 0, 0.3)",
+        "box-shadow": "0 0 2px black"
+
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+});
+
+// $(document).ready(function() {
+//   // navbar transition jQuery script
+//   $(window).scroll(function(e){
+//     if ($(this).scrollTop() > 200) {
+//       $(".navbar").slideUp();
+//     }
+//     else {
+//       $(".navbar").slideDown();
+//     }
+//   });
+// });
